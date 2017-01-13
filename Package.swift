17636fb37578495116d5cd0d3c67811778f2c3a1
Jonathan Guthrie <jono@perfect.org>
2017-01-13 14:09:36 -0500

    update logging dep

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2016-12-08 19:52:37 +0000
+// Date: 2017-01-13 19:07:26 +0000
 import PackageDescription
 let package = Package(
     name: "PerfectTurnstileCouchDBDemo",
     targets: [],
     dependencies: [
         .Package(url: "https://github.com/PerfectlySoft/Perfect-Turnstile-CouchDB.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 1, minor: 0),
     ]
 )
